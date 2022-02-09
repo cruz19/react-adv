6296da4d4e798d95e2cee47f1e9c55d3e5e8e14f
Yhon Steven Cruz <ycruz@evolution-it.com.co>
2022-02-08 19:21:25 -0500

    Componentes Lazy agregados

---
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -0,0 +1,6 @@
+
+export const LazyPage2 = () => {
+    return (
+        <h1>lazyPage 2</h1>
+    )
+}
---
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -0,0 +1,6 @@
+
+export const LazyPage3 = () => {
+    return (
+        <h1>lazyPage 3</h1>
+    )
+}
