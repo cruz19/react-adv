9657f91ccc301d97f4254766ef977ed654225e6c
Yhon Steven Cruz <ycruz@evolution-it.com.co>
2022-06-13 19:23:28 -0500

    Component compound ready

---
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,33 @@
+import { ProductCard, ProductImage, ProductTitle, ProductButtons } from "../components";
+
+const product = {
+  id: '1',
+  title: 'Coffe Mug - Card',
+  img: './coffee-mug.png'
+};
+
+export const ShoppingPage = () => {
+  return (
+    <div>
+        <h1>Shopping Store</h1>
+        <hr />
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap'
+        }}>
+          <ProductCard product={product}>
+            <ProductCard.Image />
+            <ProductCard.Title title={'Hols Mundo'} />
+            <ProductCard.Buttons />
+          </ProductCard>
+
+          <ProductCard product={product}>
+            <ProductImage />
+            <ProductTitle />
+            <ProductButtons  />
+          </ProductCard>
+        </div>
+    </div>
+  )
+}
